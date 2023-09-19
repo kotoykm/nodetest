4624abb76f9ef437cf3e02d1f39de38d5c26bb0e
Erick Rivera <yomellamoerick.771@gmail.com>
2023-09-19 07:38:41 -0300

    lesson 4 ready

---
+++ index.js
@@ -0,0 +1,19 @@
+// Como lanzar mensajes por la consola del navegador pero en mi compu
+console.log("Esto del npm start es lo que viene preconfigurado en react no te creo que se puede personalizar de forma manual desde el package.json .____.")
+
+// Aqui iria el grueso del proyecto si tuviera uno...
+
+/*
+Se puede
+comentar
+igualmente
+que en
+CSS
+pero no
+parece
+recomendable
+
+*/
+
+// Y encima no requiere el punto y coma XDDD ;;;;;;;;
+console.log("Adios baiiii")
---
+++ pringao.js
@@ -0,0 +1,2 @@
+// Con node realmente podemos ejectutar todo el javascript del mundo desde el lado del servidor que ordinario .____.
+console.log("EL HOLA MUNDO ES PARA PRINGAOS JAJAJAJAJAJAJAJAJAJA");
